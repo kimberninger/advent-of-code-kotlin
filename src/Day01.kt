b1587a3782de0671886d80f3b41b29f8b8135fe4
Kim Berninger <code@berninger.kim>
2025-12-01 12:19:38 +0100

    feat: solve day 1 puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,38 @@
 fun main() {
+    fun solution(input: List<String>, initialPosition: Int = 50, dialSize: Int = 100, chunkSize: (Int) -> Int): Int {
+        var position = initialPosition
+        var password = 0
+
+        for (line in input) {
+            val direction = line.take(1)
+            val amount = line.drop(1).toInt()
+
+            (1..amount).chunked(chunkSize(amount)).forEach { chunk ->
+                when (direction) {
+                    "L" -> position = (position - chunk.size) % dialSize
+                    "R" -> position = (position + chunk.size) % dialSize
+                }
+
+                if (position == 0) {
+                    password += 1
+                }
+            }
+        }
+        return password
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return solution(input) { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return solution(input) { 1 }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
